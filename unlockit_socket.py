9dcda791deb74e2315fa177327f72480d2c6415f
jonahcancio <jonahcancio@gmail.com>
2019-05-15 18:06:33 +0800

    added lcd and decrement

--- unlockit_socket.py
+++ unlockit_socket.py
@@ -1,9 +1,11 @@
 import RPi.GPIO as GPIO
 from mfrc522 import SimpleMFRC522
 from gpiozero import LED
+import I2C_LCD_driver
 
 import signal
 import time
+import datetime
 
 import requests
 
@@ -28,7 +30,7 @@ class Timeout():#class for timeout of input
 # actuators
 reader = SimpleMFRC522()
 unlockitSocket = LED(21)
-
+mylcd = I2C_LCD_driver.lcd()
 
 # card values
 currentCard = 0
@@ -36,40 +38,47 @@ registeredCard = 0
 timeLoggedIn = 0
 loginBalance = 0
 balanceLeft = 0
-
-# flags
-isLoginPatching = False
-isLogoutPatching = False
-
+studentName = ""
+studentNumber = 0
+decrement = 0
 
 
 def attempt_login(url):
-    global timeLoggedIn, loginBalance
+    global timeLoggedIn, loginBalance, studentName, studentNumber
     print("attempting login...")
     response = requests.patch(url, json={'is_using': True})    
     print(response.json())
     if response.status_code == 200:
-        print("LOGIN SUCCESS: your card is valid")
+        print("Valid card found")
         timeLoggedIn = time.time()
         loginBalance = response.json()['balance']
-        actuate_login() 
+        studentName = response.json()['name']
+        actuate_login()
     else:
         print("LOGIN FAILED: invalid card")
+        mylcd.lcd_display_string("Unregistered Card!", 1)
 
 
 def actuate_login():
-    global balanceLeft, registeredCard, unlockitSocket
+    global balanceLeft, registeredCard, decrement, unlockitSocket
     balanceLeft = loginBalance
-    registeredCard = currentCard
+    decrement = 0
+    mylcd.lcd_clear()
+    mylcd.lcd_display_string("Hello {}!".format(studentName), 1)
     if balanceLeft > 0:
+        registeredCard = currentCard
         unlockitSocket.on()
+    else:
+        mylcd.lcd_display_string("You have no load :(", 2)
 
 def attempt_logout(url):
-    print("attempting logut...")
-    response = requests.patch(url, json={'balance': int(balanceLeft), 'is_using': False})
+    global decrement
+    print("attempting logout...")
+    actuate_logout()
+    print("decrement: {}".format(decrement))
+    response = requests.patch(url, json={'decrement': int(decrement), 'is_using': False})
     print(response.json())
     if response.status_code == 200:
-        actuate_logout()
         print("LOGOUT SUCCESS")
     else:
          print("LOGOUT FAILED")
@@ -78,13 +87,17 @@ def actuate_logout():
     global registeredCard, loginBalance, balanceLeft
     registeredCard = 0
     loginBalance = 0
-    balanceLeft = 0
     unlockitSocket.off()
-    
+    mylcd.lcd_clear()
+    mylcd.lcd_display_string("Bye {}!".format(studentName), 1)
+    print_lcd_balance(balanceLeft)
 
+def print_lcd_balance(seconds):
+    balanceOutput = datetime.timedelta(seconds=seconds)
+    mylcd.lcd_display_string("Balance: {}".format(balanceOutput), 2)
 
 def main():
-    global currentCard, registeredCard, timeLoggedIn, balanceLeft
+    global currentCard, registeredCard, timeLoggedIn, balanceLeft, decrement
     while True:
         try:
             with Timeout(1):
@@ -96,6 +109,8 @@ def main():
         except:
             if currentCard == 0:
                 print("No Card")
+                mylcd.lcd_clear()
+                mylcd.lcd_display_string("No Card", 1)
         finally:
             GPIO.cleanup()
         
@@ -111,15 +126,17 @@ def main():
         
         # handle balance calculations
         if registeredCard > 0:
-            balanceLeft = loginBalance - (time.time() - timeLoggedIn)
+            decrement = time.time() - timeLoggedIn
+            balanceLeft = loginBalance - decrement
             balanceLeft = balanceLeft if balanceLeft > 0 else 0
             print("Balance left: {}".format(balanceLeft))
+            print_lcd_balance(balanceLeft)
             if balanceLeft <= 0:
                 print("YOUR TIME IS UP, PINHEAD!")
+                mylcd.lcd_clear()
+                mylcd.lcd_display_string("YOUR TIME IS UP!", 2)
                 attempt_logout('https://unlockitsocketapp.herokuapp.com/api/simple/students/{}/'.format(registeredCard))
-            
-            
-        
+
 main()
 
 
